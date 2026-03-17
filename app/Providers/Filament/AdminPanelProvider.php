cdf4198ffdf9c0c93ca28f88cc2d41ee2e89863b
davelozaMJ <daveloza@grupomainjobs.com>
2026-03-17 15:54:14 +0100

    Faltaba añadir este ignore y se muestran las notificaciones

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -57,6 +57,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->maxContentWidth(Width::Full);
+            ->maxContentWidth(Width::Full)
+            ->notifications();
     }
 }
